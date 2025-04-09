a69a08d3ee0a50f92803e6d3a172b1df48d4bb5a
Duncan <duncan.pronk@wearejust.com>
2025-04-09 12:21:43 +0200

    fix: dont throw error in CP

--- src/Helper.php
+++ src/Helper.php
@@ -14,19 +14,23 @@ use Statamic\Facades\Markdown;
 /*
  *  Generate HTML from content
  */
-if (! function_exists('generateHtml')) {
-  function generateHtml($content)
-  {
-    if (is_array($content)) {
-      return  (new \Tiptap\Editor)->setContent([
-        'type' => 'doc',
-        'content' => $content,
-      ])->getHTML();
-    }
-    if (Markdown::hasParser($content)) {
-      return Markdown::parser($content);
-    }
 
-    return $content;
-  }
+if (! function_exists('generateHtml')) {
+    function generateHtml($content)
+    {
+        if (is_array($content)) {
+            return (new \Tiptap\Editor)->setContent([
+                'type' => 'doc',
+                'content' => $content,
+            ])->getHTML();
+        }
+        try {
+            if (Markdown::hasParser($content)) {
+                return Markdown::parser($content);
+            }
+        } catch (\TypeError) {
+        }
+
+        return $content;
+    }
 }
